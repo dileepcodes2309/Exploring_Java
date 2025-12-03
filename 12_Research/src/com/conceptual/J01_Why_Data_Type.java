aba342c1b7932e1cd13832cfd16026aae5e26157
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-04 03:14:06 +0530

    why data type is importamt

---
+++ 12_Research/src/com/conceptual/J01_Why_Data_Type.java
@@ -0,0 +1,14 @@
+package com.conceptual;
+
+public class J01_Why_Data_Type {
+    public static void main(String[] args) {
+
+        String whyDataType = """
+                Data type is important in Java because Java is strongly typed
+                language and we need to tell compiler that what is the type of the
+                data.
+                """;
+
+        System.out.println(whyDataType);
+    }
+}
