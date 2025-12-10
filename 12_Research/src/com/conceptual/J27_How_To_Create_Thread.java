58e8984d84bb4c1d93a9e6caf6f67b1f90d45f44
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-10 05:30:54 +0530

    how to create threads

---
+++ 12_Research/src/com/conceptual/J27_How_To_Create_Thread.java
@@ -0,0 +1,22 @@
+package com.conceptual;
+
+public class J27_How_To_Create_Thread {
+    public static void main(String[] args) {
+
+        String howToCreate = """
+                There are two ways to create thread in Java.
+                1.By extending Thread class.
+                2.By implementing the Runnable interface.
+                """;
+
+        String preferenceInRealWorld = """
+                In real project we prefer Runnable interface for creating threads because
+                in Java we can extend only class but can implement multiple interfaces.
+                """;
+
+        String twoThread = """
+                Yes, we can create multiple Thread objects and pass the same Runnable.
+                Each thread will run its own run() but share the same object state.
+                """;
+    }
+}
