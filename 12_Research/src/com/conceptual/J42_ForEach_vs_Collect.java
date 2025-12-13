733cda193cbe2953a362997fdf1683875c5a3822
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-14 03:04:47 +0530

    forEach() vs collect

---
+++ 12_Research/src/com/conceptual/J42_ForEach_vs_Collect.java
@@ -0,0 +1,17 @@
+package com.conceptual;
+
+public class J42_ForEach_vs_Collect {
+    public static void main(String[] args) {
+
+        String collect = """
+                -> It collects the processed stream into data structure (like a List,Set or Map)
+                -> It returns a data structure.
+                -> We use when want ti store or reuse the data.
+                """;
+
+        String foreach = """
+                -> It go through each item and perform an action.
+                -> It return nothing.-> 
+                """;
+    }
+}
