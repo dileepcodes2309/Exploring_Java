fa4e7980abe0cf497b45e1d64a25eed841e09976
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-13 11:30:59 +0530

    Exception

---
+++ 12_Research/src/com/conceptual/J31_ExceptionHandling.java
@@ -0,0 +1,23 @@
+package com.conceptual;
+
+public class J31_ExceptionHandling {
+    public static void main(String[] args) {
+
+        String definition = """
+                -> Exception is unexcepted event that disturb the normal flow of the program.
+                -> We can handle exceptions using a structured mechanism using keyword: try,
+                   catch and finally.
+                """;
+
+        String tryWithoutCatch = """
+                -> We cannot write only try block. Try block must be followed by cath or finally block.
+                """;
+
+        String exceptionHierarchy = """
+                -> At the top is throwable which is the root of all errors and exception in Java.
+                -> Then comes two branch -: Error and Exception
+                """;
+
+
+    }
+}
