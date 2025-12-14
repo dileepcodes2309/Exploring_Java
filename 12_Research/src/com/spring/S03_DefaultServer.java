e02833b75776b036185a683c5e06eda57d780e25
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-14 11:36:36 +0530

    deafult server

---
+++ 12_Research/src/com/spring/S03_DefaultServer.java
@@ -0,0 +1,11 @@
+package com.spring;
+
+public class S03_DefaultServer {
+    public static void main(String[] args) {
+
+        String defaultServer = """
+                -> Default Embedded Server in Spring Boot Apache Tomcat.
+                -> Comes by default with spring-boot-starter-web
+                """;
+    }
+}
