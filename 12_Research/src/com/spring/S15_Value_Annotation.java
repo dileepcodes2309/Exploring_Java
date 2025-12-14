07898e73926909f1c3442e71770c351131f6af4f
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-14 23:26:45 +0530

    Value annotation

---
+++ 12_Research/src/com/spring/S15_Value_Annotation.java
@@ -0,0 +1,11 @@
+package com.spring;
+
+public class S15_Value_Annotation {
+    public static void main(String[] args) {
+
+        String whenToUse = """
+                We use @Value annotation to inject values from application.properties file
+                into our code.
+                """;
+    }
+}
