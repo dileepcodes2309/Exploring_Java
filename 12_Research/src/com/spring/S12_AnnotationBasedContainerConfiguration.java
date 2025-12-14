442f7afef09dbf65113f7891074c3a4ff0d254ea
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-14 23:06:06 +0530

    Annotation based configuration

---
+++ 12_Research/src/com/spring/S12_AnnotationBasedContainerConfiguration.java
@@ -0,0 +1,14 @@
+package com.spring;
+
+public class S12_AnnotationBasedContainerConfiguration {
+    public static void main(String[] args) {
+
+        String explanation = """
+                -> Before annotation based configuration, we were using XML based config
+                   which wsd lengthy and hard to maintain.
+                -> Now Instead of XML files, we can configure Spring beans using annotations.
+                   This is called annotation-based configuration.
+                -> Some common annotations are @Bean, @Component, @ComponentScan etc
+                """;
+    }
+}
