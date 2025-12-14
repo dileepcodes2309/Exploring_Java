bcfb8b5b005967304068171d6054aeb9f2ce0d55
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-14 22:42:23 +0530

    Application.Properties

---
+++ 12_Research/src/com/spring/S09_ApplicationProperties.java
@@ -0,0 +1,12 @@
+package com.spring;
+
+public class S09_ApplicationProperties {
+    public static void main(String[] args) {
+
+        String applicationProperties = """
+                -> We store all our application configuration in application.properties.
+                -> Instead of hardcoding values, we put them there.
+                -> So they can be easily changed without changing the code.
+                """;
+    }
+}
