c947e8e3205a9933598552c187b9bcf02b94793c
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-14 15:22:58 +0530

    Dependency Injection

---
+++ 12_Research/src/com/spring/S06_DependencyInjection.java
@@ -0,0 +1,12 @@
+package com.spring;
+
+public class S06_DependencyInjection {
+    public static void main(String[] args) {
+
+        String dependencyInjection = """
+                -> It is an design pattern which says the objects don't need to create their  own
+                   dependencies instead container create the required dependencies.
+                -> It makes them loosely coupled, easier to maintain and easier to Unit test.
+                """;
+    }
+}
