86e429842d38626d960adb6a32425010811a0304
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-15 01:37:53 +0530

    ORM

--- 12_Research/src/com/spring/S23_ORM.java
+++ 12_Research/src/com/spring/S23_ORM.java
@@ -7,7 +7,7 @@ public class S23_ORM {
                 -ORM stands for Object Relation Mapping.
                 -It automatically maps the Class to a table
                 -An object to a row.
-                -Field to a column.
+                -Field to a column.  
                 """;
     }
 }
