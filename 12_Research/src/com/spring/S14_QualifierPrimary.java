f52e202719f812d6ebb750829da55a1ed10017a4
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-14 23:22:59 +0530

    Qualifer and Primary annotaion

---
+++ 12_Research/src/com/spring/S14_QualifierPrimary.java
@@ -0,0 +1,11 @@
+package com.spring;
+
+public class S14_QualifierPrimary {
+    public static void main(String[] args) {
+
+        String whyWeUse = """
+                -> In Spring, Qualifier and Primary annotation are use to resolve ambiguity
+                   when multiple beans of same type are available for dependency injection.
+                """;
+    }
+}
