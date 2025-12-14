ad1598dc8323cfaeefdfaf70348065b7e5207602
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-15 01:10:22 +0530

    Spring Bean

---
+++ 12_Research/src/com/spring/S19_Spring_Bean.java
@@ -0,0 +1,14 @@
+package com.spring;
+
+public class S19_Spring_Bean {
+    public static void main(String[] args) {
+
+        String springBean = """
+                -> A java class which is managed by the IOC container is called as Spring Bean.
+                -> The life cycle of the spring bean is taken care by the IOC container.
+                -> Spring beans are instantiated, configured, wired, and managed by IoC container.
+                -> Beans are created with the configuration metadata that the users supply to the container
+                   (by means of XML or java annotations configurations.)
+                """;
+    }
+}
