2ce0a9870ea95ea86a4fcc591b171b558033c831
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-14 23:08:44 +0530

    Autowired annotation

---
+++ 12_Research/src/com/spring/S13_Autowired.java
@@ -0,0 +1,12 @@
+package com.spring;
+
+public class S13_Autowired {
+    public static void main(String[] args) {
+
+        String autoWired = """
+                -> The @Autowired annotation in Spring is a mechanism for automatic dependency injection.
+                -> It allows the Spring IoC (Inversion of Control) container to automatically resolve and inject dependencies into the Spring-managed components (beans)
+                   without requiring explicit configuration in XML or Java.
+                """;
+    }
+}
