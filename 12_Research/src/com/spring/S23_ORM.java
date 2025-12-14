e9d5e64a1a21b3a1ecbd35d3d1b88948e914a08b
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-15 01:37:34 +0530

    ORn

---
+++ 12_Research/src/com/spring/S23_ORM.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+public class S23_ORM {
+    public static void main(String[] args) {
+
+        String explanation = """
+                -ORM stands for Object Relation Mapping.
+                -It automatically maps the Class to a table
+                -An object to a row.
+                -Field to a column.
+                """;
+    }
+}
