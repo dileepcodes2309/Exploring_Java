0e06d9535c378afb7b93937b26b1b60894cca26f
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-16 02:03:56 +0530

    Enable Spring Security

---
+++ 12_Research/src/com/spring/S30_Enabling_Spring_Security.java
@@ -0,0 +1,12 @@
+package com.spring;
+
+public class S30_Enabling_Spring_Security {
+    public static void main(String[] args) {
+
+        String howToEnable = """
+                -> Add Spring Security dependency.
+                -> Once this on classpath, Spring configure a basic security setup.
+                -> Then, all our endpoints will be secured unless explicitly allowed.
+                """;
+    }
+}
