2204fb33fc832574dc968b0ad37b582eaf2bfb87
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-16 02:15:02 +0530

    EnableWebConfig

---
+++ 12_Research/src/com/spring/S31_EnableWebSecurity.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+public class S31_EnableWebSecurity {
+
+    String enableWebSecurity = """
+            -> It enables Spring Security’s web security support and allows you
+               to define custom security configuration.
+            """;
+
+    String whenToUse = """
+            -> When we want to override the default configuration we will use EnableWebSecurity.
+            """;
+}
