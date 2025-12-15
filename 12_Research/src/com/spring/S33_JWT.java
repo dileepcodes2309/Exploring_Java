a62e1c3e6fd8b2db8ce4693f1f5ede32022734e1
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-16 03:52:19 +0530

    JWT

---
+++ 12_Research/src/com/spring/S33_JWT.java
@@ -0,0 +1,14 @@
+package com.spring;
+
+public class S33_JWT {
+
+    public static void main(String[] args) {
+        String jwt = """
+                -> JWT - JSON Web Tokens
+                -> Instead of sending the username and password every time,
+                   the server gives the client a token after a successful login.
+                -> This token is a signed string containing encoded information about
+                   the user (like their username and roles).
+                """;
+    }
+}
