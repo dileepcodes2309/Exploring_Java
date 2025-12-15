5b8a7961b09f888e1ad2a45ba2b402b3c324d681
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-16 00:20:40 +0530

    Checking permission

---
+++ 12_Research/src/com/spring/S29_Spring_Security.java
@@ -0,0 +1,21 @@
+package com.spring;
+
+public class S29_Spring_Security {
+    public static void main(String[] args) {
+
+        String springSecurity = """
+                -> It is a framework that protect our application.
+                -> It is just like a bodyguard for our application.
+                -> Whenever a request come to our application, it doest not allow that request
+                   to walk freely.
+                """;
+
+        String authentication = """
+                -> Verifying the identity.
+                """;
+
+        String authorization = """
+                -> Checking permission.
+                """;
+    }
+}
