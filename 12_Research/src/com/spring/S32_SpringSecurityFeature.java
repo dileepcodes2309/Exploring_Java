5ad3b7eac646f68a2a793fc89303300956baf303
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-16 03:38:36 +0530

    EnableWebConfig

---
+++ 12_Research/src/com/spring/S32_SpringSecurityFeature.java
@@ -0,0 +1,21 @@
+package com.spring;
+
+public class S32_SpringSecurityFeature {
+    public static void main(String[] args) {
+
+        //Spring Security features
+        String authorization = """
+                -> It is the process of validating the identity of a user who is accessing the
+                   application.
+                """;
+
+        String athorization = """
+                -> It is a process of restricting the user to access a particular part of an application.
+                """;
+
+        String commonAttacks = """
+                -> Spring Security automatically protects against many common vulnerabilities like
+                   CSRF and XSS.
+                """;
+    }
+}
