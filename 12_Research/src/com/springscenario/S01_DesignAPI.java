b797e74a189e25daf8049b09df324d476ec21c7c
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-18 01:08:37 +0530

    designing an API to fetch the user details

---
+++ 12_Research/src/com/springscenario/S01_DesignAPI.java
@@ -0,0 +1,17 @@
+package com.springscenario;
+
+public class S01_DesignAPI {
+    public static void main(String[] args) {
+
+        /*
+        You are designing an API to fetch the user details.
+        How would you like to fetch, will you use user-id or user-email and why?
+         */
+
+        String answer = """
+                I will prefer to use user id because:
+                ->Each user has an unique ID which is usually auto-generated integer or UUID.
+                  There is no chance of collision.
+                """;
+    }
+}
