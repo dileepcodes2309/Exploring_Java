a442226eaf288163315bb329b263f7aca46ce0c4
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-17 12:15:27 +0530

    Java Object to JSON

---
+++ 12_Research/src/com/springrest/R06_JavaObject_To_JSON.java
@@ -0,0 +1,11 @@
+package com.springrest;
+
+public class R06_JavaObject_To_JSON {
+    public static void main(String[] args) {
+
+        String behindTheScene = """
+                Spring converts Java objects to JSON using Jackson via HttpMessageConverters 
+                when returning responses from @RestController methods.
+                """;
+    }
+}
