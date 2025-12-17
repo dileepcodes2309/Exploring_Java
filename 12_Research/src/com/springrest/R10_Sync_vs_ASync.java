62fef14f3bdae2b98c5de0186273aa0335978573
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-18 00:49:28 +0530

    Sync vs ASync

---
+++ 12_Research/src/com/springrest/R10_Sync_vs_ASync.java
@@ -0,0 +1,13 @@
+package com.springrest;
+
+public class R10_Sync_vs_ASync {
+
+    String sync = """
+            In synchronous communication, the client sends a request and waits until the
+            server fully processes it and returns a response.
+            """;
+
+    String async = """
+            In asynchronous communication, the client does not wait for the server to finish processing.
+            """;
+}
