bdef45c9e8d09a7b2b2c2211c5c5243800c25838
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-20 16:17:21 +0530

    wait, notify and notifyAll

--- 12_Research/src/com/java/J30_Synchronization.java
+++ 12_Research/src/com/java/J30_Synchronization.java
@@ -9,5 +9,28 @@ public class J30_Synchronization {
                 ->With the help of Synchronization, it ensures that only one thread can execute a
                   critical section of code.
                 """;
+
+        String waitForSomeone = """
+                -> In multithreading, some threads often wait for some event to happen.
+                   Here, wait, notify and notifyAll comes in picture.
+                
+                Real life analogy:
+                Suppose in a phone booth, you are talking to a friend over phone and others are
+                waiting outside.
+                When you finish your job, you say its done and next person will go in.
+                
+                That is wait() (someone waiting), and notify() (someone getting informed that they can go in).
+                """;
+
+        String wait = """
+                -> It causes the current thread to release its lock so that other thread can work on the object.
+                """;
+
+        String notify = """
+                -> It wakes up one thread that is waiting on the same object
+                """;
+        String notifyAll = """
+                -> wakes up all waiting threads (only one can proceed at a time)
+                """;
     }
 }
