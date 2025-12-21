6368e218a4ee14d0adf186befa6802a8d6364926
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-22 00:50:41 +0530

    oop

---
+++ 12_Research/src/com/java/J00_OOP.java
@@ -0,0 +1,16 @@
+package com.java;
+
+public class J00_OOP {
+    public static void main(String[] args) {
+        String oop = """
+                OOP is a programming paradigm that guides how we write Java code by organizing it into classes
+                and objects, combining data and behaviour.
+                
+                OOP tells us:
+                Use class to represent real-world entities.
+                Use object create instances.
+                
+                PAIE- Polymorphism, Abstraction, Inheritance and Encapsulation
+                """;
+    }
+}
