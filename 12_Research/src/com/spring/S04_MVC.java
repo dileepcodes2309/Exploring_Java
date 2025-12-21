881c50a2553fd3a484c5be7dd60c9734b648843a
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-21 16:32:59 +0530

    MVC

---
+++ 12_Research/src/com/spring/S04_MVC.java
@@ -0,0 +1,32 @@
+package com.spring;
+
+public class S04_MVC {
+    public static void main(String[] args) {
+        String explanation = """
+                1️⃣ Model
+                Represents business data and logic\\
+                Interacts with the database
+                Examples:
+                Entity classes
+                DTOs
+                Service logic
+                
+                Model = data + business rules
+                
+                2️⃣View
+                Represents the UI
+                Displays data to the user
+                Examples:
+                JSP
+                Thymeleaf
+                HTML pages
+                
+                3️⃣Controller
+                Handles user requests
+                Acts as a middleman between View and Model
+                Examples:
+                @Controller
+                @RestController
+                """;
+    }
+}
