74c0a3801600dfda35ed298aa391d45823b63dd1
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-24 08:34:27 +0530

    Microservices Architecture

---
+++ 12_Research/src/com/microservices/M01_Architectural_Style.java
@@ -0,0 +1,21 @@
+package com.microservices;
+
+public class M01_Architectural_Style {
+    public static void main(String[] args) {
+
+        String whatIs = """
+                -> Architectural Style defines, how a system is structured at high level.
+                -> It gives us a "big picture" view of the system - like looking at its
+                   from a helicopter.
+                -> It shows how a system is designed and how its major part are communicate with each
+                   other.
+                """;
+
+        String examples = """
+                Microservices Architecture
+                Example:
+                Order, Payment, User as separate services
+                Communicate via REST APIs
+                """;
+    }
+}
