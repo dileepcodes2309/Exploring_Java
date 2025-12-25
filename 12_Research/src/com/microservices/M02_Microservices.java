1b82db929fdf5e7bcaa9292762c8f5c74e420ba2
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-25 23:59:39 +0530

    Microservices definition

---
+++ 12_Research/src/com/microservices/M02_Microservices.java
@@ -0,0 +1,12 @@
+package com.microservices;
+
+public class M02_Microservices {
+    public static void main(String[] args) {
+
+        String explanation = """
+                -> Microservices is one of the architectural style available that we can
+                   consider for building a web application.
+                -> Microservices are independent services designed around business domains and deployed separately.
+                """;
+    }
+}
