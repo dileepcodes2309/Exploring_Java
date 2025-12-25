bc8805ac4e392d6c2c7f16c0191fea16749702e7
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-26 00:05:09 +0530

    Microservices definition

--- 12_Research/src/com/microservices/M02_Microservices.java
+++ 12_Research/src/com/microservices/M02_Microservices.java
@@ -8,5 +8,15 @@ public class M02_Microservices {
                    consider for building a web application.
                 -> Microservices are independent services designed around business domains and deployed separately.
                 """;
+
+        String formalDefinition = """
+                ->Microservices is an approach to building a single application as a collection of small services
+                  Each service:
+                ->Runs in its own process
+                ->Is independently deployable
+                ->Services communicate using lightweight mechanisms (such as REST or messaging)
+                ->Each service is designed around a specific business capability
+                ->Deployment is handled through fully automated deployment pipelines
+                """;
     }
 }
