a39bf4f74cae002781c74747a917b502eb1be455
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-26 00:48:53 +0530

    Springboot actuator

---
+++ 12_Research/src/com/microservices/M03_Actuator.java
@@ -0,0 +1,16 @@
+package com.microservices;
+
+public class M03_Actuator {
+    public static void main(String[] args) {
+
+        String whyWeUse = """
+                Helps detect service failures
+                
+                Enables health-based load balancing
+                
+                Integrates with monitoring tools (Prometheus, Grafana)
+                
+                Supports auto-scaling decisions
+                """;
+    }
+}
