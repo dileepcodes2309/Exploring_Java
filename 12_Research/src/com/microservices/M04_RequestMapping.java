2e53ea2288c4f9ecf9485860898c3f3aa242c342
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-26 13:46:40 +0530

    @RequestMapping working

---
+++ 12_Research/src/com/microservices/M04_RequestMapping.java
@@ -0,0 +1,12 @@
+package com.microservices;
+
+public class M04_RequestMapping {
+    public static void main(String[] args) {
+
+        String role = """
+                -> @RequestMapping is used to map HTTP requests (URLs + HTTP methods) to
+                   specific controller methods.
+                -> It tells Spring which method should handle which request.
+                """;
+    }
+}
