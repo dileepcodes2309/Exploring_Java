f0744b9f8009ce9532c4d6b03ff926181fb57414
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-29 01:46:15 +0530

    docker file info

---
+++ 12_Research/src/com/microservices/M10_DockerFile_Creation.java
@@ -0,0 +1,32 @@
+package com.microservices;
+
+public class M10_DockerFile_Creation {
+    public static void main(String[] args) {
+
+        //Steps to follow
+        String dockerFile = """
+                -> Create a file named as Dockerfile.
+                -> Inside dockerfile we write instructions that can be used to
+                   create docker image.
+                
+                """;
+
+        String dockerFileInfo = """
+                #Start with a base image containning JAVA runtime
+                FROM eclipse-temurin:17-jdk-jammy
+                
+                #Information around who manage the image
+                LABEL "org.opencontainers.image.authors"="dileep"
+                
+                #Add application's jar to the image
+                COPY target/accounts.jar accounts.jar
+                
+                #execute the application
+                ENTRYPOINT["java","-jar","accounts.jar"]
+                """;
+
+        String dockerContainerCreation = """
+                -> docker run -d -p 8080:8080 dileep2309/accounts:d29
+                """;
+    }
+}
