76bd85d10a3b9c7cd8c71b4e6ee547edeb650fa4
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-29 22:14:47 +0530

    environment variable

---
+++ 12_Research/src/com/microservices/M12_Environment.java
@@ -0,0 +1,20 @@
+package com.microservices;
+
+public class M12_Environment {
+    public static void main(String[] args) {
+
+        String environment = """
+                Environment is a Spring Core interface that provides access to:
+                
+                ->application.properties
+                
+                ->application.yml
+                
+                ->OS environment variables
+                
+                ->JVM system properties
+                
+                ->Active Spring profiles
+                """;
+    }
+}
