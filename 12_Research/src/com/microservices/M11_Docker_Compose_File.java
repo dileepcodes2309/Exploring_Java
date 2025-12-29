e2912c292fde60bf7e7e284db5fe559526ac9c73
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-29 21:13:26 +0530

    docker compose file

---
+++ 12_Research/src/com/microservices/M11_Docker_Compose_File.java
@@ -0,0 +1,11 @@
+package com.microservices;
+
+public class M11_Docker_Compose_File {
+    public static void main(String[] args) {
+
+        String whatIsIt = """
+                A Docker Compose file (docker-compose.yml) is a YAML configuration file
+                used to define and run multiple Docker containers together.
+                """;
+    }
+}
