b7865cc25e40530c42c394ce159bf9a0c77ab3d2
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-31 03:08:19 +0530

    accessing properties through file system

---
+++ 12_Research/src/com/microservices/M17_Storing_File_In_File_System.java
@@ -0,0 +1,11 @@
+package com.microservices;
+
+public class M17_Storing_File_In_File_System {
+    public static void main(String[] args) {
+
+        String changesToDo = """
+                Add below property in Config=Server yml file
+                search-locations: "file:///C://Users//dilee//Desktop//December//Microservices//config"
+                """;
+    }
+}
