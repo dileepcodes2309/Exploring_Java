9b359af046ec09fe2c25b95d0bc8388164c0ed2a
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-30 21:43:59 +0530

    Sprimg cloud config

---
+++ 12_Research/src/com/microservices/M14_Spring_Cloud_Config.java
@@ -0,0 +1,14 @@
+package com.microservices;
+
+public class M14_Spring_Cloud_Config {
+
+    public static void main(String[] args) {
+
+        String whatIs = """
+                -> Spring Cloud Config is a centralized configuration management solution
+                   for Spring Boot / microservices.
+                -> It allows all your microservices to fetch configuration from one central place instead
+                   of having their own application.yml.
+                """;
+    }
+}
