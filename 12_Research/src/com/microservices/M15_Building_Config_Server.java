5805d9f1873adef3735790f7959f999f9b085336
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2025-12-30 22:02:21 +0530

    Build config server

---
+++ 12_Research/src/com/microservices/M15_Building_Config_Server.java
@@ -0,0 +1,13 @@
+package com.microservices;
+
+public class M15_Building_Config_Server {
+    public static void main(String[] args) {
+
+        String howToBuild = """
+                -> Create a maven spring project.
+                -> Add Config Server dependency
+                -> Add actuator also.
+                -> Add @EnableConfigServer annotation in main class.
+                """;
+    }
+}
