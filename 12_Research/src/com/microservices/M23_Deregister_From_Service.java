64e249b8c99038bec6f8edbc7b4657dca09c5ccb
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2026-01-02 03:12:43 +0530

    deregister from serveice registery

---
+++ 12_Research/src/com/microservices/M23_Deregister_From_Service.java
@@ -0,0 +1,11 @@
+package com.microservices;
+
+public class M23_Deregister_From_Service {
+    public static void main(String[] args) {
+
+        String url = """
+                Use below url as post request:
+                http://localhost:8081/actuator/shutdown
+                """;
+    }
+}
