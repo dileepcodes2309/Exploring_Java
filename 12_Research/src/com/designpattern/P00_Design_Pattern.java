ecfe7a04b8092e0807a99fb47d4e5ae190ea0046
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2026-01-06 02:09:19 +0530

    design pattern meaning

---
+++ 12_Research/src/com/designpattern/P00_Design_Pattern.java
@@ -0,0 +1,11 @@
+package com.designpattern;
+
+public class P00_Design_Pattern {
+    public static void main(String[] args) {
+
+        String meaning = """
+                -> A design pattern is a reusable, proven solution for common problems in software design
+                -> It is like a blueprint that explain how to solve a problem in particular solution.
+                """;
+    }
+}
