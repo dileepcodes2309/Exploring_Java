b7eba93892d13640d6e909c2d237fb5de4d21853
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2026-01-07 03:41:14 +0530

    Singleton class

---
+++ 12_Research/src/com/designpattern/P01_Singleton.java
@@ -0,0 +1,31 @@
+package com.designpattern;
+
+public class P01_Singleton {
+
+    public static void main(String[] args) {
+
+        String meaning = """
+                -> A singleton class has only one instance that can be accessible globally through a single
+                   entry point.
+                """;
+
+        String implementation = """
+                -> There are two options for implementing singleton.
+                -> Eager and Lazy singleton.
+                """;
+
+        String stepsForEager = """
+                -> Create a private constructor so that no one can create object.
+                -> Create a static final variable holding the object.
+                -> Create public getter for global access to that object.
+                """;
+
+        String stepsForLazy = """
+                -> Create a private constructor.
+                -> Create a static volatile of reference of type of same class so that it read the latest
+                   value from main memory not from cache.
+                -> Create public getter for global access to that object.
+                -> Make sure to make that block synchronized to prevent duplicate object.
+                """;
+    }
+}
