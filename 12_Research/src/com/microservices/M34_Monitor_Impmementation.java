d42120516c3e591da3e1755abd7a887a1a9c7fcd
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2026-01-08 04:27:42 +0530

    grafana config

---
+++ 12_Research/src/com/microservices/M34_Monitor_Impmementation.java
@@ -0,0 +1,13 @@
+package com.microservices;
+
+public class M34_Monitor_Impmementation {
+
+    public static void main(String[] args) {
+
+        String howToImplement = """
+                -> Download all the yml file.
+                -> Create a folder named observality inside docker compose.
+                -> http://localhost:3000/
+                """;
+    }
+}
