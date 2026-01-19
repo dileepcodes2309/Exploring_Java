def7a31a4a3a2a294ee9109b4bad3c9874a8e308
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2026-01-20 02:25:28 +0530

    What is single page application

---
+++ 12_Research/src/com/angular/A01_SinglePageApplication.java
@@ -0,0 +1,12 @@
+package com.angular;
+
+public class A01_SinglePageApplication {
+    public static void main(String[] args) {
+
+        String singlePageApplication = """
+                -> In Single Page Application, the page loads only once and after that its contents
+                   loads dynamically.
+                -> Example: Gmail-> Click email → page does NOT refresh
+                """;
+    }
+}
