9f8a0da993de08bc3e00067c5eed989592da1ae2
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2026-01-20 04:15:02 +0530

    Module

---
+++ 12_Research/src/com/angular/A05_Angular_Module.java
@@ -0,0 +1,11 @@
+package com.angular;
+
+public class A05_Angular_Module {
+    public static void main(String[] args) {
+
+        String whatIsIt = """
+                -> Module in angular is a class with decorative @NgModule.
+                -> @NgModule will contain the declaration of Component, Pipe, Service etc.
+                """;
+    }
+}
