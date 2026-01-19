7ee45f75d20cc206224327588e1eccf8c6117bc9
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2026-01-20 04:16:37 +0530

    Command for executing Angular aplication

---
+++ 12_Research/src/com/angular/A06_Executing_Angular_Application.java
@@ -0,0 +1,11 @@
+package com.angular;
+
+public class A06_Executing_Angular_Application {
+
+    public static void main(String[] args) {
+
+        String command = """
+                ng serve --open
+                """;
+    }
+}
