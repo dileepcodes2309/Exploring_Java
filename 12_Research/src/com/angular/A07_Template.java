21ec079f843049bc799a551a168743c5b65442c5
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2026-01-21 03:27:56 +0530

    Angular template

---
+++ 12_Research/src/com/angular/A07_Template.java
@@ -0,0 +1,11 @@
+package com.angular;
+
+public class A07_Template {
+    public static void main(String[] args) {
+
+        String angularTemplate = """
+                -> In angular, template is the html part of the component.
+                -> Template is the HTML that defines the view (UI) of a component.
+                """;
+    }
+}
