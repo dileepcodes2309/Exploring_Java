03756972ff1d9c280d8f64421ced2ce9a7382c07
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2026-01-25 03:47:16 +0530

    pipe in angular

---
+++ 12_Research/src/com/angular/A10_Pipes.java
@@ -0,0 +1,10 @@
+package com.angular;
+
+public class A10_Pipes {
+    public static void main(String[] args) {
+
+        String pipe = """
+                In Angular, a Pipe is used to transform data before displaying it in the template.
+                """;
+    }
+}
