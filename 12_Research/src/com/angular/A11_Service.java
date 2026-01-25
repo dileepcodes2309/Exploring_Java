a33a8817a57416b4ba2d3f30d79085d7a4d5a386
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2026-01-26 02:02:11 +0530

    Service

---
+++ 12_Research/src/com/angular/A11_Service.java
@@ -0,0 +1,14 @@
+package com.angular;
+
+public class A11_Service {
+    public static void main(String[] args) {
+
+        String whatIsService = """
+                -> A service in Angular is a class used to share logic or data across multiple components.
+                """;
+
+        String observable = """
+                An Observable is a stream of data that you can subscribe to and receive values over time.
+                """;
+    }
+}
