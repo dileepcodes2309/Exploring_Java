9808694d59f3ff45f2c2d394fe2f540597a2efc4
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2026-01-26 18:18:16 +0530

    Microservice advantage

---
+++ 12_Research/src/com/microservice_systemdesign/M02_Advantage_Of_Microservice.java
@@ -0,0 +1,16 @@
+package com.microservice_systemdesign;
+
+public class M02_Advantage_Of_Microservice {
+    public static void main(String[] args) {
+
+        String advantage = """
+                -> In microservice environment, every microservice is small and its easier to understand.
+                -> If one microservice needs an update then only that part is touched.
+                -> Different teams can work on different teams services independently which makes the
+                   development faster.
+                -> If some microservices receives more traffic then only those microservices needs to be
+                   scaled.
+                -> As each service is small which is easier to manage and testing becomes easy.
+                """;
+    }
+}
