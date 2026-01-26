f9053fb8893d50bd88198d5c5e2c7567ebd510cd
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2026-01-27 00:06:16 +0530

    Side card pattern

---
+++ 12_Research/src/com/microservice_systemdesign/M07_Side_Car_Pattern.java
@@ -0,0 +1,11 @@
+package com.microservice_systemdesign;
+
+public class M07_Side_Car_Pattern {
+    public static void main(String[] args) {
+
+        String meaning = """
+                -> Sidecar pattern is about attaching a helper service to the main service.
+                -> Helper service adds extra service without changing the code in the main service
+                """;
+    }
+}
