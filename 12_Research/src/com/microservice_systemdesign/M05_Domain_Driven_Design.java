dca33461a78ac777efc9c2299d6f47ada29ad84d
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2026-01-26 23:17:48 +0530

    Domain Drivedn Design pattern

---
+++ 12_Research/src/com/microservice_systemdesign/M05_Domain_Driven_Design.java
@@ -0,0 +1,11 @@
+package com.microservice_systemdesign;
+
+public class M05_Domain_Driven_Design {
+    public static void main(String[] args) {
+
+        String meaning = """
+                -> It is a way of building systems where focus stays on business problems first and
+                   code second.
+                """;
+    }
+}
