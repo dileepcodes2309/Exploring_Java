5b5caa8f07572098f394ff4b395a6a5c8af0f34c
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2026-01-26 18:09:37 +0530

    Microservice definition

---
+++ 12_Research/src/com/microservice_systemdesign/M01_Microservice.java
@@ -0,0 +1,15 @@
+package com.microservice_systemdesign;
+
+public class M01_Microservice {
+    public static void main(String[] args) {
+
+        String definition = """
+                -> It is an architectural style available in the market that we can consider for
+                   building a web application.
+                -> Microservice is a way to build a single web application as a collection of small
+                   services.
+                -> Each service has its own job.
+                -> Each service can be deployed independently.
+                """;
+    }
+}
