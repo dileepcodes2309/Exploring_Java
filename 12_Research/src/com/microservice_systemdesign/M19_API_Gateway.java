7c34fb60ecece9244eae926950a3e4d3c71c20e5
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2026-01-29 04:07:28 +0530

    API gateway

---
+++ 12_Research/src/com/microservice_systemdesign/M19_API_Gateway.java
@@ -0,0 +1,16 @@
+package com.microservice_systemdesign;
+
+public class M19_API_Gateway {
+
+    String whatIs = """
+            -> API gateway acts as single ntry point for all the requests.
+            """;
+
+    String analogy = """
+            An API Gateway works like the single main entrance to a building. Instead of letting everyone enter
+            through different doors, we create one main door. Inside the building, people can go to different rooms.
+            Similarly, the gateway takes the client’s request and sends it to the right microservice.
+            """;
+
+
+}
