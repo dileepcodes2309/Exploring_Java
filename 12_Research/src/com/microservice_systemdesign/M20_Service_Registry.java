ff89e6db890b4ccec81ac786d7af1f5435948729
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2026-01-29 05:49:50 +0530

    Service registry

---
+++ 12_Research/src/com/microservice_systemdesign/M20_Service_Registry.java
@@ -0,0 +1,13 @@
+package com.microservice_systemdesign;
+
+public class M20_Service_Registry {
+
+    public static void main(String[] args) {
+
+        String serviceRegistry = """
+                -> A service registry is a central directory where microservices register themselves
+                   and discover other services.
+                -> It’s like a phonebook for microservices.
+                """;
+    }
+}
