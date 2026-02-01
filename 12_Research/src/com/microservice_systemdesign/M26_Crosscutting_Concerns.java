5f40b56fab437d23f2d4705fa7882d2e5031f2f2
Dileep Kumar Behera <126802368+Dileep-Develeper2309@users.noreply.github.com>
2026-02-02 00:18:12 +0530

    cross cutting concerns

---
+++ 12_Research/src/com/microservice_systemdesign/M26_Crosscutting_Concerns.java
@@ -0,0 +1,13 @@
+package com.microservice_systemdesign;
+
+public class M26_Crosscutting_Concerns {
+
+    public static void main(String[] args) {
+
+        String whatIs = """
+                -> In microservice world, every microservice has its own job but apart from their
+                   main responsibility they have some common task like error handling, configuration
+                   management which is common to and these are called cross cutting concerns.
+                """;
+    }
+}
